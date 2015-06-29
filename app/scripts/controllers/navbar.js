c9886ed03455d48b6eebba635aad8588a7d52d4f
bmacheski <bwm0007@gmail.com>
2015-06-29 15:21:42 -0500

    move navbar to own file and set up logout

---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular
+	.module('tutHubApp')
+	.controller('NavbarCtrl', function ($rootScope, $location, Auth){
+		var vm = this;
+		vm.logout = function (){
+			Auth.$unauth();
+			$rootScope.auth = null;
+			$location.path('/login');
+		}
+	})

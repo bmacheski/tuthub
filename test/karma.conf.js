088b6eac299fed41cd07804c5f20cd222efab177
bmacheski <bwm0007@gmail.com>
2015-07-04 13:01:58 -0500

    remove bootstrap and add create topic form

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,7 +24,6 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/materialize/bin/materialize.js',
       'bower_components/firebase/firebase.js',
